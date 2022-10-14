746a4e29e94bfb43939565a4e74f0faec2a9d6dc
jinsumin <suminjin@naver.com>
2022-10-14 15:59:53 +0900

    video player component

--- pages/sections/viewer.js
+++ pages/sections/viewer.js
@@ -2,11 +2,57 @@ import React from "react";
 import { Carousel } from "flowbite-react";
 
 const Viewer = () => {
+  // const videoRef = useRef();
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     videoRef.current.play();
+  //   }, 5000);
+  // }, []);
+
+  const videoProperties = [
+    {
+      id: 1,
+      title: "video1",
+      src: "videos/hana-1q-celebration.mp4",
+      description: "sample video of 1q wedding",
+    },
+    {
+      id: 2,
+      title: "video2",
+      src: "videos/hana-1q-wedding-simulation.mp4",
+      description: "sample video of 1q wedding",
+    },
+    {
+      id: 3,
+      title: "video3",
+      src: "videos/hana-1q-wedding-celebration.mp4",
+      description: "sample video of 1q wedding",
+    },
+  ];
+
   return (
     <div>
-      <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <div className="sm:h-64 xl:h-80 2xl:h-96 mx-56">
         <Carousel>
-          <img
+          {videoProperties.map((videoObject) => {
+            return (
+              <div key={videoObject.id} className="place-items-center">
+                <video
+                  // ref={videoRef}
+                  controls
+                  width="100%"
+                  height="100%"
+                  loop
+                  muted
+                  src={videoObject.src}
+                />
+                <h3>{videoObject.title}</h3>
+                <p>{videoObject.description}</p>
+              </div>
+            );
+          })}
+          {/* <img
             src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
             alt="..."
           />
@@ -25,7 +71,7 @@ const Viewer = () => {
           <img
             src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
             alt="..."
-          />
+          /> */}
         </Carousel>
       </div>
     </div>
