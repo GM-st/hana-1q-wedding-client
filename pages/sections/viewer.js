b093e99a8332b044027729da182c9c102e99f284
jinsumin <suminjin@naver.com>
2022-10-14 14:42:22 +0900

    test

---
+++ pages/sections/viewer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Viewer = () => {
+    return (
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Viewer;
