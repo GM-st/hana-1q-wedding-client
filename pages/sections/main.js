d935cf68e8f1ec75192e99dc7d127ca748a7d723
jinsumin <suminjin@naver.com>
2022-10-18 15:08:31 +0900

    deploy

--- pages/sections/main.js
+++ pages/sections/main.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import Animation from "../../components/animation";
-import Qrcode from "../../components/qrcode";
 
 const Main = () => {
   const appStoreURL =
