ef006648ac471ba3bd2dbe90882425a83d1babd9
jinsumin <suminjin@naver.com>
2022-10-19 23:27:44 +0900

    player page video style

--- pages/player.js
+++ pages/player.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Layout from "../components/layout";
 
 const Player = () => {
   const [videoFilePath, setVideoFilePath] = useState(null);
@@ -8,28 +9,33 @@ const Player = () => {
   };
 
   return (
-    <div id="player-section">
-      <section className="text-gray-600 body-font">
-        <div className="container flex justify-center mx-20 mt-20">
-          <div className="flex-col items-center place-items-center">
-            <div>
-              <input type="file" onChange={handleVideoUpload} />
+    <>
+      <Layout>
+        <div id="player-section" className="bg-teal-800">
+          <section className="text-gray-600 body-font">
+            <div className="flex justify-center mx-20 py-20">
+              <div className="flex-col items-center place-items-center">
+                <div>
+                  <input type="file" onChange={handleVideoUpload} />
+                </div>
+                <div className="max-w-3xl">
+                  <video
+                    // ref={videoRef}
+                    controls
+                    width="100%"
+                    height="100%"
+                    // loop
+                    // muted
+                    src={videoFilePath}
+                    poster="hana-3d-logo.png"
+                  />
+                </div>
+              </div>
             </div>
-            <div className="max-w-3xl">
-              <video
-                // ref={videoRef}
-                controls
-                width="100%"
-                height="100%"
-                // loop
-                // muted
-                src={videoFilePath}
-              />
-            </div>
-          </div>
+          </section>
         </div>
-      </section>
-    </div>
+      </Layout>
+    </>
   );
 };
 
