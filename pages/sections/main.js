2998b11705b02d8a260f0ecb726d382e59edbb51
jinsumin <suminjin@naver.com>
2022-10-21 01:25:57 +0900

    main title comment

--- pages/sections/main.js
+++ pages/sections/main.js
@@ -14,8 +14,9 @@ const Main = () => {
     <div id="main-section" className="bg-gray-100">
       <div className="relative flex items-center justify-center h-screen overflow-hidden">
         <div className="relative z-30 p-5 text-white bg-opacity-50 rounded-xl mb-60 items-center">
-          <h1 className="flex flex-row text-black text-4xl items-center justify-center text-center leading-tight sm:text-6xl md:text-6xl">
-            하나 1Q 웨딩에서<br></br>결혼식 모든 절차를 한번에
+          <h1 className="flex flex-col text-black text-4xl items-center justify-center text-center leading-sung sm:text-6xl md:text-6xl">
+            <p className="mb-3">한큐에 끝내는</p>
+            <p>1Q Wedding</p>
           </h1>
           <div className="flex flex-col md:flex-row sm:flex-row lg:flex-row items-center justify-center mt-20">
             <div class="flex flex-row items-center justify-center bg-black w-52 h-12 rounded-lg m-3 shadow-lg mt-10 bg-opacity-80 hover:bg-teal-500 hover:opacity-80 hover:shadow-lg focus:bg-teal-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-teal-800 active:shadow-lg transition duration-150 ease-in-out">
